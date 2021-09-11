015778ff50467965d6c84e8723205a304017b150
vlad <devmaffi550@gmail.com>
2021-09-11 20:14:56 +0300

    Building Movies component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Movies from './components/movies';
+
 export class App extends Component {
   render() {
     return (
       <main className="container">
-        <h1>Hello Vidly!</h1>
+        <Movies />
       </main>
     );
   }
