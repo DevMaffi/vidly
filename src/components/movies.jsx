44ba28ded3eb4dd0cdc6f7325d3a0bfcfc8277eb
vlad <devmaffi550@gmail.com>
2021-09-15 21:30:36 +0300

    Move movies table to Table component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Like from './common/like';
+import Table from './table';
 import ListGroup from './common/listGroup';
 import Pagination from './common/pagination';
 
@@ -72,42 +72,11 @@ export class Movies extends Component {
         <div className="col">
           <p>Showing {count > 1 ? `${count} movies` : '1 movie'} in database</p>
 
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th />
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              {movies.map(movie => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like
-                      onLikeToggle={() => this.handleLike(movie)}
-                      liked={movie.liked}
-                    />
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => this.handleDelete(movie)}
-                      className="btn btn-danger btn-sm"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <Table
+            movies={movies}
+            onLikeToggle={this.handleLike}
+            onDelete={this.handleDelete}
+          />
 
           <Pagination
             itemsCount={count}
