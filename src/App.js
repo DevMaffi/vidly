664e35d9f2cd4ae6d1368ca15e97c253381711ff
vlad <devmaffi550@gmail.com>
2021-09-23 15:03:44 +0300

    Linking to the MovieForm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 import NavBar from './components/navBar';
 import Movies from './components/movies';
+import MovieForm from './components/movieForm';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
@@ -14,6 +15,7 @@ export class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/movies/:id" component={MovieForm}></Route>
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
             <Route path="/rentals" component={Rentals}></Route>
--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-function MovieForm() {
-  return <h1>MovieForm</h1>;
+function MovieForm(props) {
+  const { match, history } = props;
+
+  return (
+    <div>
+      <h1>MovieForm {match.params.id}</h1>
+      <button
+        onClick={() => history.push('/movies')}
+        className="btn btn-primary"
+      >
+        Save
+      </button>
+    </div>
+  );
 }
 
 export default MovieForm;
