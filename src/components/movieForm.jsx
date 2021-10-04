fb2b5279c03e2ce15f0f15571e223940e7c5145d
vlad <devmaffi550@gmail.com>
2021-10-04 17:02:52 +0300

    Implementing saveMovie into the MovieService

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -64,8 +64,8 @@ class MovieForm extends Form {
     dailyRentalRate: movie.dailyRentalRate,
   });
 
-  doSubmit = () => {
-    saveMovie(this.state.data);
+  doSubmit = async () => {
+    await saveMovie(this.state.data);
 
     this.props.history.push('/movies');
   };
