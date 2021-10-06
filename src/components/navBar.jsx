e528e71a01c1f1983eddae0d4d4b6e5ce141b8b4
vlad <devmaffi550@gmail.com>
2021-10-06 16:53:43 +0300

    Displaying the current user on NavBar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
-function NavBar() {
+function NavBar(props) {
+  const { user } = props;
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
@@ -36,16 +38,34 @@ function NavBar() {
                 Rentals
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/login">
-                Login
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/register">
-                Register
-              </NavLink>
-            </li>
+            {!user && (
+              <>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/login">
+                    Login
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/register">
+                    Register
+                  </NavLink>
+                </li>
+              </>
+            )}
+            {user && (
+              <>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/profile">
+                    {user.name}
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/logout">
+                    Log out
+                  </NavLink>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
