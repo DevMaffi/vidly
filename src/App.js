5646bf6e5769877c319a2b3420161014cc7efe65
vlad <devmaffi550@gmail.com>
2021-10-07 15:13:44 +0300

    Refactoring App Routes

--- src/App.js
+++ src/App.js
@@ -34,14 +34,14 @@ export class App extends Component {
         <NavBar user={this.state.user} />
         <main className="container">
           <Switch>
-            <Route path="/login" component={LoginForm}></Route>
-            <Route path="/logout" component={Logout}></Route>
-            <Route path="/register" component={RegisterForm}></Route>
-            <Route path="/movies/:id" component={MovieForm}></Route>
-            <Route path="/movies" component={Movies}></Route>
-            <Route path="/customers" component={Customers}></Route>
-            <Route path="/rentals" component={Rentals}></Route>
-            <Route path="/not-found" component={NotFound}></Route>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
             <Redirect from="/" exact to="movies" />
             <Redirect to="/not-found" />
           </Switch>
